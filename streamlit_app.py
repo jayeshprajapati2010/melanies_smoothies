c9a6edd7587c8f955cd1dc6e53259f75110b7a70
jayeshprajapati2010 <jayesh.prajapati2010@gmail.com>
2024-12-13 15:45:45 -0500

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,65 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+# Write directly to the app
+st.title(":cup_with_straw: Customize your Smoothie!")
+st.write(
+    """Choose the Fruits you want in your smoothie!
+    """)
+
+
+# Get the current credentials
+session = get_active_session()
+
+customer_name = st.text_input('Name on Smoothie')
+#st.write(customer_name)
+
+fruit_data = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT_NAME'))
+#st.dataframe(data = fruit_data,use_container_width=True)
+
+ingrrdiant_list = st.multiselect(
+    'Choose upto 5 fruits',
+    fruit_data,
+    max_selections = 5
+)
+
+sunmit_order = st.button('Submit Order')
+
+if ingrrdiant_list and customer_name:
+    #st.write('Your selected : ',ingrrdiant_list)
+    #st.text(ingrrdiant_list)
+    
+    ingredients_string =  ''
+
+    for each_fruit in ingrrdiant_list:
+        ingredients_string += each_fruit + ',';
+
+    #st.write('Your selected : ',ingredients_string)
+
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,NAME_ON_ORDER)
+            values ('""" + ingredients_string + """','"""+customer_name +"""')"""
+
+    #st.write(my_insert_stmt)
+
+    if ingredients_string and sunmit_order:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered, '+ customer_name +'!', icon="✅")
+        st.stop()
+
+
+    
+
+#contact = st.selectbox(
+#    'How would you like to be contected?',
+#    ('Email','Home Phone','Mobile Phone'))
+
+
+#fruit = st.selectbox(
+#    'What is your favourite Fruit?',
+#    ('Mango','Orange','Apple','Banana','Staberies','Peaches'))
+
+
+
+
