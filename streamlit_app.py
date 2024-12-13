36d20a5e9307d882d3baf6421b697597a87fe867
jayeshprajapati2010 <jayesh.prajapati2010@gmail.com>
2024-12-13 15:57:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -11,7 +11,9 @@ st.write(
 
 
 # Get the current credentials
-session = get_active_session()
+#session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 customer_name = st.text_input('Name on Smoothie')
 #st.write(customer_name)
