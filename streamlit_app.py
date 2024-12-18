12593c2991b29473ca6225ee3a698c7523452a76
jayeshprajapati2010 <jayesh.prajapati2010@gmail.com>
2024-12-18 14:21:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ fruit_data = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT_N
 
 # convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df = fruit_data.to_pandas()
-st.dataframe(pd_df)
+#st.dataframe(pd_df)
 #st.stop()
 
 ingrrdiant_list = st.multiselect(
