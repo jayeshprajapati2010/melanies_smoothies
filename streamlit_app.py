a0ae241394d2b3ef4096bf6833998dcda51d6812
jayeshprajapati2010 <jayesh.prajapati2010@gmail.com>
2024-12-18 14:27:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,9 +43,9 @@ if ingrrdiant_list and customer_name:
         ingredients_string += each_fruit + ' ';
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == each_fruit, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', each_fruit,' is ', search_on, '.')
+        #st.write('The search value for ', each_fruit,' is ', search_on, '.')
         
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+each_fruit)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+search_on)
         st.text(each_fruit + ' Nutrision Intormation')
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
