3a9270dd4720ef55c4aef18a22b7f4146937f025
jayeshprajapati2010 <jayesh.prajapati2010@gmail.com>
2024-12-18 13:45:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,8 +50,8 @@ if ingrrdiant_list and customer_name:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_wifth=True)
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     
 
 #contact = st.selectbox(
